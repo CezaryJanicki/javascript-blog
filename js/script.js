92d7626f8bbd00616b7cd0e710968227a964617e
CezaryJanicki <cezary.janicki@gmail.com>
2023-03-15 17:36:17 +0100

    Remove console.log

--- js/script.js
+++ js/script.js
@@ -30,50 +30,39 @@ const titleClickHandler = function(event){
     targetArticle.classList.add("active");
   }
 
-    const optArticleSelector = '.post',
+  const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
 const generateTitleLinks = function generateTitleLinks(){
+
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
     titleList.innerHTML = '';
 
     /* find all the articles and save them to variable: articles */
     const articles = document.querySelectorAll(optArticleSelector)
-    console.log("articles are " + articles);
-    console.log(articles);
     let html = '';
 
     for(let article of articles){
+
       /* get the article id */
       const articleId = article.getAttribute("id");
-      console.log("article id is " + articleId);
-      console.log(articleId);
 
       /* find the title element */
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
       /* get the title from the title element */
-      console.log("Article title is " + articleTitle);
-      console.log(articleTitle);
       /* create HTML of the link */
       const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      console.log("link html is " + linkHTML);
-      console.log(linkHTML);
 
       /* insert link into html variable */
       html = html + linkHTML;
-      console.log(html)
-      console.log("-------------------------------------")
     }
 
     titleList.innerHTML = html;
 
     const links = document.querySelectorAll('.titles a');
-    console.log(links);
-    console.log("LINKS TO " + links);
-
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
     }
